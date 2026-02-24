dfa2e9f941709be349437c94dae64cab64a9e874
admin <user@host.myguest.virtualbox.org>
2026-02-24 10:50:25 -0600

    update script

--- bash_practice_stuff.sh
+++ bash_practice_stuff.sh
@@ -22,11 +22,45 @@ if [ $user_input -lt 5 ]; then
 
 	elif [[ $user_input == 5 ]]; then
 		echo 'your input was equal to 5
-		double [[]] brackets were used for regular expression'
+double [[]] brackets were used for regular expression'
 
 	else 
 		echo 'your input was greater than 5'
 
 fi
 
+demonstration_of_a_function_while_loop(){
+
+read -p "Lets look at while loops. Insert a number for x that is a solution to the equation
+99 + x = 100
+While your answer is incorrect, it will prompt you to enter a different number
+Enter solution while_loop_variable_input
+: " while_loop_variable_input
+double_round_bracket_example=$((99+while_loop_variable_input))
+while [[ $double_round_bracket_example != 100 ]]; do
+	read -p 'sum of numbers doesnt equal 100. Enter a different answer
+: ' while_loop_variable_input
+	double_round_bracket_example=$((99+while_loop_variable_input))
+done
+
+}
+demonstration_of_a_function_while_loop
+
+case_statements(){
+	read -p "Enter input: " input
+	#input="A"
+	case $input in
+		A|a)
+			echo "execute this text - matches A" ;;
+		B|b)
+			echo "Matched B" ;;
+		*)
+			echo "didnt match" ;;
+	esac
+}
+case_statements
 #note to self: look at command injection bc that will be interesting!
+
+#CMD LINE INJECTION W/ ECHO
+injection=data
+read -p "User Input Entry" input_test
